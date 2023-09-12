5376e2b86d28840be91f9275078764a14a286ee8
Andrew Barrett <andrew.barrett@peoplestrategy.com>
2023-09-11 21:12:47 -0500

    forgot src folder

---
+++ src/app.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { getLanguages, getProjects, getProjectLanguages } from "./database.js";
+
+const app = express();
+
+const port = process.env.API_PORT || 8080;
+
+app.use(function (_, res, next) {
+  if (process.env.ENVIRONMENT == "dev") {
+    res.header("Access-Control-Allow-Origin", "*");
+  } else {
+    res.header("Access-Control-Allow-Origin", "https://abarrett.io");
+  }
+  next();
+});
+
+app.get("/", async function (_, res) {
+  res.send("API server is working");
+});
+
+app.get("/languages", async (_, res) => {
+  const languages = await getLanguages();
+  res.json(languages);
+});
+
+app.get("/projects", async (_, res) => {
+  const projects = await getProjects();
+  res.json(projects);
+});
+
+app.get("/project_languages", async (_, res) => {
+  const projectLanguages = await getProjectLanguages();
+  res.json(projectLanguages);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}
+  `);
+});
---
+++ src/database.js
@@ -0,0 +1,56 @@
+import mysql from "mysql2";
+
+const pool = mysql
+  .createPool({
+    host: process.env.DB_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
+    typeCast: function castField(field, useDefaultTypeCasting) {
+      // We only want to cast bit fields that have a single-bit in them. If the field
+      // has more than one bit, then we cannot assume it is supposed to be a Boolean.
+      if (field.type === "BIT" && field.length === 1) {
+        var bytes = field.buffer();
+
+        // A Buffer in Node represents a collection of 8-bit unsigned integers.
+        // Therefore, our single "bit field" comes back as the bits '0000 0001',
+        // which is equivalent to the number 1.
+        return bytes[0] === 1;
+      }
+
+      return useDefaultTypeCasting();
+    },
+  })
+  .promise();
+
+async function getProjects() {
+  const [rows] = await pool.query(`
+select projects.id, projects.title, projects.content, GROUP_CONCAT(languages.name SEPARATOR ', ') AS languages from projects left outer join project_langauge_relation on projects.id = project_langauge_relation.project_id left outer join languages on languages.id
+= project_langauge_relation.language_id GROUP BY projects.id;
+`);
+  return rows;
+}
+
+async function getProjectLanguages() {
+  const [rows] = await pool.query(`
+    select * from project_langauge_relation;
+    `);
+  return rows;
+}
+
+// TODO: experiences
+// async function getExperiences() {
+//   const [rows] = await pool.query(`
+//   SELECT *
+//   `);
+// }
+
+async function getLanguages() {
+  const [rows] = await pool.query(`
+  select name,photo_url,display_name from languages where display_order IS
+ NOT NULL ORDER BY display_order ASC `);
+  return rows;
+}
+
+export { getLanguages, getProjects, getProjectLanguages };
